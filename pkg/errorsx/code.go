2f94a52c1f61e3828291ae6c622cc8f2d524716e
guigliu <guigliu@tencent.com>
2025-11-10 14:23:09 +0800

    feat: gin处理请求方法

--- pkg/errorsx/code.go
+++ pkg/errorsx/code.go
@@ -6,4 +6,7 @@ import "net/http"
 var (
 	// ErrInternal 表示所有未知的服务器端错误.
 	ErrInternal = &ErrorX{Code: http.StatusInternalServerError, Reason: "InternalError", Message: "Internal server error."}
+
+	// ErrBind 表示请求体绑定错误.
+	ErrBind = &ErrorX{Code: http.StatusBadRequest, Reason: "BindError", Message: "Error occurred while binding the request body to the struct."}
 )
